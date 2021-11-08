70fb96f27fe7e64fc2a2d4d89df63b10db438c69
francelys1848 <72948506+francelys1848@users.noreply.github.com>
2021-11-08 17:15:16 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+function roster(name, pronouns, school, year, color) {
+  return (
+    <div class="row">
+      <div class="red">{name}</div>
+      <div>{pronouns}</div>
+      <div class="red">{school}</div>
+      <div>{year}</div>
+      <div class="red">{color}</div>
+    </div>
+  );
+}
+export default function App() {
+  const mikkail = roster(
+    "Mikkail Allen",
+    "He/ Him",
+    "Williamsburg Charter High School",
+    2022,
+    "rats"
+  );
+  const claudia = roster(
+    "Luna-Marcelino",
+    "She/Hers",
+    "P-TECH",
+    2022,
+    "",
+    "",
+    "Cats"
+  );
+  const francelys = roster(
+    "Francelys Lomeli ",
+    "She/Her",
+    "france",
+    2022,
+    "dogs"
+  );
+  return (
+    <div className="App">
+      {mikkail}
+      {claudia}
+      {francelys}
+    </div>
+  );
+}
